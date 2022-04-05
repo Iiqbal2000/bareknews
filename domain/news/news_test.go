b3b33947399c7c53e5338d1f27d3d2d57fe4b5d9
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2022-04-06 05:59:39 +0700

    adding test

--- domain/news/news_test.go
+++ domain/news/news_test.go
@@ -1,62 +1,110 @@
 package news_test
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/Iiqbal2000/bareknews"
-// 	"github.com/Iiqbal2000/bareknews/domain/news"
-// 	"github.com/matryer/is"
-// )
+	"github.com/Iiqbal2000/bareknews/domain"
+	"github.com/Iiqbal2000/bareknews/domain/news"
+	"github.com/google/uuid"
+	"github.com/matryer/is"
+)
 
-// func TestCreateNews(t *testing.T) {
-// 	title := "Test 1"
-// 	slug := bareknews.Slug("test-1")
-// 	body := "testing"
-// 	tag1, _ := bareknews.NewTags("tag1")
-// 	tag2, _ := bareknews.NewTags("tag2")
-// 	news, err := news.New(title, body, []bareknews.Tags{*tag1, *tag2})
+func TestCreateNews(t *testing.T) {
+	t.Run("success creates a news", func(t *testing.T) {
+		title := "Test 1"
+		slug := domain.Slug("test-1")
+		body := "testing"
+		tagid := uuid.New()
+		news := news.New(title, body, domain.Draft, []uuid.UUID{tagid})
+
+		err := news.Validate()
+
+		is := is.New(t)
+		is.NoErr(err)
+		is.Equal(news.Post.Title, title)
+		is.Equal(news.Post.Body, body)
+		is.Equal(news.Status, domain.Draft)
+		is.Equal(news.Slug, slug)
+	})
+
+	t.Run("with invalid title should return an error", func(t *testing.T) {
+		body := "testing"
+		tagid := uuid.New()
+		news := news.New("", body, domain.Draft, []uuid.UUID{tagid})
+
+		err := news.Validate()
+
+		is := is.New(t)
+		is.True(err != nil)
+	})
+
+	t.Run("with invalid status should return an error", func(t *testing.T) {
+		body := "testing"
+		tagid := uuid.New()
+		news := news.New("invalid status", body, "", []uuid.UUID{tagid})
+
+		err := news.Validate()
+
+		is := is.New(t)
+		is.True(err != nil)
+	})
+}
+
+func TestChangeTitle(t *testing.T) {
+	tagId := uuid.New()
+	news := news.New("Test 1", "testing", domain.Draft, []uuid.UUID{tagId})
+	err := news.Validate()
 	
-// 	is := is.New(t)
-// 	is.True(err == nil)
-// 	is.Equal(news.Post.Title, title)
-// 	is.Equal(news.Post.Body, body)
-// 	is.Equal(news.Post.Status, bareknews.Draft)
-// 	is.Equal(news.Slug, slug)
-// }
+	is := is.New(t)
+	is.NoErr(err)
 
-// func TestChangeTitle(t *testing.T) {
-// 	tag1, _ := bareknews.NewTags("tag1")
-// 	tag2, _ := bareknews.NewTags("tag2")
-// 	news, err := news.New("Test 1", "testing", []bareknews.Tags{*tag1, *tag2})
+	news.ChangeTitle("Test 2")
+	err = news.Validate()
+	is.NoErr(err)
+	is.Equal(news.Post.Title, "Test 2")
+	is.Equal(news.Slug, domain.Slug("test-2"))
+}
+
+func TestChangeBody(t *testing.T) {
+	tagId := uuid.New()
+	news := news.New("Test 1", "testing", domain.Draft, []uuid.UUID{tagId})
+	err := news.Validate()
+
+	is := is.New(t)
+	is.NoErr(err)
+
+	news.ChangeBody("Changing the body")
+	err = news.Validate()
+	is.NoErr(err)
+	is.Equal(news.Post.Body, "Changing the body")
+}
+
+func TestChangeStatus(t *testing.T) {
+	tagId := uuid.New()
+	news := news.New("Test 1", "testing", domain.Draft, []uuid.UUID{tagId})
+	err := news.Validate()
+
+	is := is.New(t)
+	is.NoErr(err)
+
+	news.ChangeStatus(domain.Publish)
+	err = news.Validate()
+	is.NoErr(err)
+	is.Equal(news.Status, domain.Publish)
+}
+
+func TestChangeTags(t *testing.T) {
+	tagId := uuid.New()
+	news := news.New("Test 1", "testing", domain.Draft, []uuid.UUID{tagId})
+	err := news.Validate()
+
+	is := is.New(t)
+	is.NoErr(err)
 	
-// 	is := is.New(t)
-// 	is.True(err == nil)
+	newTag := uuid.New()
 
-// 	news.ChangeTitle("Test 2")
-// 	is.Equal(news.Post.Title, "Test 2")
-// 	is.Equal(news.Slug, bareknews.Slug("test-2"))
-// }
-
-// func TestChangeBody(t *testing.T) {
-// 	tag1, _ := bareknews.NewTags("tag1")
-// 	tag2, _ := bareknews.NewTags("tag2")
-// 	news, err := news.New("Test 1", "testing", []bareknews.Tags{*tag1, *tag2})
-	
-// 	is := is.New(t)
-// 	is.True(err == nil)
-
-// 	news.ChangeBody("Changing the body")
-// 	is.Equal(news.Post.Body, "Changing the body")
-// }
-
-// func TestChangeStatus(t *testing.T) {
-// 	tag1, _ := bareknews.NewTags("tag1")
-// 	tag2, _ := bareknews.NewTags("tag2")
-// 	news, err := news.New("Test 1", "testing", []bareknews.Tags{*tag1, *tag2})
-	
-// 	is := is.New(t)
-// 	is.True(err == nil)
-
-// 	news.ChangeStatus(int(bareknews.Publish))
-// 	is.Equal(news.Post.Status, bareknews.Publish)
-// }
+	news.ChangeTags([]uuid.UUID{newTag})
+	err = news.Validate()
+	is.NoErr(err)
+	is.Equal(news.TagsID[0], newTag)
+}
