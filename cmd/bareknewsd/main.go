4c32aded6e389cb82d04e255dda55ada59f3f961
Iiqbal2000 <iqbalhafizh2000@gmail.com>
2022-04-12 23:20:04 +0700

    fixing http server

--- cmd/bareknewsd/main.go
+++ cmd/bareknewsd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/Iiqbal2000/bareknews/infrastructure/handler"
 	"github.com/Iiqbal2000/bareknews/infrastructure/storage"
+	"github.com/Iiqbal2000/bareknews/services/posting"
 	"github.com/Iiqbal2000/bareknews/services/tagging"
 	"github.com/go-chi/chi/v5"
 )
@@ -15,22 +18,38 @@ func main() {
 	r := chi.NewRouter()
 
 	dbConn := storage.Run(dbfile, true)
-	// newsDB := storage.News{Conn: dbConn}
+	newsDB := storage.News{Conn: dbConn}
 	tagDB := storage.Tag{Conn: dbConn}
 
 	taggingSvc := tagging.New(tagDB)
-	// postingSvc := posting.New(newsDB, taggingSvc)
+	postingSvc := posting.New(newsDB, taggingSvc)
 
 	tagHandler := handler.Tags{Service: taggingSvc}
-	
-	r.Use(func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("content-type", "application/json")
-			h.ServeHTTP(w, r)
-		})
-	})
+	newsHandler := handler.News{Service: postingSvc}
+
+	r.Use(ContentTypeJSON)
 
 	r.Route("/tags", tagHandler.Route)
+	r.Route("/news", newsHandler.Route)
 
-	http.ListenAndServe(":3333", r)
+	address := ":3333"
+
+	s := &http.Server{
+		Addr:         address,
+		Handler:      r,
+		ReadTimeout:  time.Duration(5 * time.Second),
+		WriteTimeout: time.Duration(10 * time.Second),
+		IdleTimeout:  time.Duration(15 * time.Second),
+	}
+
+	log.Printf("Starting server at %s\n", address)
+
+	s.ListenAndServe()
+}
+
+func ContentTypeJSON(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json;charset=utf8")
+		h.ServeHTTP(w, r)
+	})
 }
